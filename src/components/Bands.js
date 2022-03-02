b785be771431e4cc0a42453052f2ab7fbafcb513
amy-chinlai <amy@actionnetwork.org>
2022-03-02 16:27:16 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 const Bands = props => {
     console.log("props below")
     console.log(props.bands)
-    const bands = props.bands.map(band => <Band key={band.id} {...band} />)
+    const bands = props.bands.map(band => <Band key={band.id} name={band.name} />)
 
     return (
         <div>
